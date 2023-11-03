c62d2f8ba1601f594de5cc4a9b6a6d6801cca00f
Rajarshi Sarkar <rajsarkar799@gmail.com>
2023-11-03 13:23:21 +0530

    Refine styles in Tailwind CSS for improved readability

--- src/components/birthday.jsx
+++ src/components/birthday.jsx
@@ -4,14 +4,14 @@ import List from "./list";
 function Birthday() {
   const [people, setPeople] = useState(data);
   return (
-    <main className="bg-yellow-600 h-screen flex justify-center">
-      <section className=" bg-white m-24 rounded-lg">
-        <h3 className="text-2xl text-center my-6 capitalize font-bold">
+    <main className="bg-yellow-600 h-screen flex justify-center ">
+      <section className="bg-white m-20 rounded-lg shadow-xl shadow-black">
+        <h3 className="text-2xl text-center my-10 capitalize font-bold">
           {people.length} birthdays today
         </h3>
         <List people1={people} />
         <button
-          className="text-white bg-indigo-500 w-96 p-2 mx-10 my-8 text-lg rounded cursor-grabbing tracking-widest hover:bg-slate-800 shadow-md shadow-slate-800"
+          className="text-white bg-indigo-500 w-96 p-2 mx-10 my-16 text-lg rounded cursor-grabbing tracking-widest hover:bg-slate-800 shadow-md shadow-slate-800"
           onClick={() => setPeople([])}
         >
           Snap All!
--- src/components/list.jsx
+++ src/components/list.jsx
@@ -6,6 +6,7 @@ const List = ({ people1 }) => {
       {people1.map((person) => {
         const { id, name, age, image } = person;
         return (
+          // Individual person's information
           <article key={id} className="h-28 w-96 mx-10 flex items-center">
             <img
               className="w-20 h-20 rounded-full object-cover mr-6"
@@ -13,7 +14,7 @@ const List = ({ people1 }) => {
               alt={name}
             />
             <div>
-              <h4 className="mb-2 tracking-widest leading-5 font-bold">
+              <h4 className="text-lg mb-2 tracking-widest leading-5 font-bold font-serif">
                 {name}
               </h4>
               <p className="text-slate-700">{age} years</p>
